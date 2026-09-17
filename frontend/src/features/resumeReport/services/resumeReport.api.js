2c7dca5e0f0972d0ceb15c1c15906d9befccf09e
Abhishek Kamati <kamtiabhi4@gmail.com>
2026-09-18 01:06:16 +0530

    url fixes

--- frontend/src/features/resumeReport/services/resumeReport.api.js
+++ frontend/src/features/resumeReport/services/resumeReport.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/interview",
+  baseURL: "https://interview-strategy-planner-backend.vercel.app/api/interview",
   withCredentials: true,
 });
 
